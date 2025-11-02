e9536c6ef22f8cbc34ab352449700ae9b7915964
A. I. LAILABA <abdullahiibrahima5126@gmail.com>
2025-11-02 08:19:00 +0100

    Update
    Update

---
+++ pre-req.sh
@@ -0,0 +1,2 @@
+sudo apt update
+sudo apt install -y live-build debootstrap qemu-user-static binfmt-support squashfs-tools xorriso isolinux syslinux-utils grub-pc-bin grub-efi-amd64-bin grub-efi-arm64-bin calamares curl chattr
