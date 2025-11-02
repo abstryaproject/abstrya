2293387d0f06b6cf4139c4776ddaa5d0891fa2e1
A. I. LAILABA <abdullahiibrahima5126@gmail.com>
2025-11-02 14:50:30 +0100

    Update

--- pre-req.sh
+++ pre-req.sh
@@ -3,4 +3,4 @@ set -euo pipefail
 IFS=$'\n\t'
 
 sudo apt update
-sudo apt install -y live-build debootstrap qemu-user-static binfmt-support squashfs-tools xorriso isolinux syslinux-utils grub-pc-bin grub-efi-amd64-bin grub-efi-arm64-bin calamares curl chattr
+sudo apt install -y live-build debootstrap qemu-user-static binfmt-support squashfs-tools xorriso isolinux syslinux-utils grub-pc-bin grub-efi-amd64-bin grub-efi-arm64-bin calamares curl chattr xorg openbox lightdm chromium-browser lxterminal network-manager network-manager-gnome nm-connection-editor zenity tor privoxy wget git certbot python3-certbot-nginx python3-certbot-apache
