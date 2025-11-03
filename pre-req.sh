a9bc147c838ac81ebf30e26e88c19a765eb4a73c
A. I. LAILABA <abdullahiibrahima5126@gmail.com>
2025-11-03 06:36:42 +0100

    Update
    Update

--- pre-req.sh
+++ pre-req.sh
@@ -1,6 +1,19 @@
-#!/system/bin/sh
+#!/bin/bash
 set -euo pipefail
-IFS=$'\n\t'
 
-sudo apt update
-sudo apt install -y live-build debootstrap qemu-user-static binfmt-support squashfs-tools xorriso isolinux syslinux-utils grub-pc-bin grub-efi-amd64-bin grub-efi-arm64-bin calamares curl chattr xorg openbox lightdm chromium-browser lxterminal network-manager network-manager-gnome nm-connection-editor zenity tor privoxy wget git certbot python3-certbot-nginx python3-certbot-apache
+# Ensure script is run as root
+if [ "$(id -u)" -ne 0 ]; then
+  echo "❗ Run this script as root (sudo su -)"
+  exit 1
+fi
+
+apt update && apt upgrade -y
+
+apt install -y \
+  live-build debootstrap qemu-user-static binfmt-support squashfs-tools xorriso \
+  isolinux syslinux-utils grub-pc-bin grub-efi-amd64-bin grub-efi-arm64-bin \
+  calamares curl xorg openbox lightdm chromium lxterminal \
+  network-manager network-manager-gnome nm-connection-editor \
+  zenity tor privoxy wget git \
+  certbot python3-certbot-nginx python3-certbot-apache \
+  e2fsprogs
